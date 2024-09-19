6f6bb2510a486aa5fba30f707707e4f7cdf490ae
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-09-19 21:07:59 +0200

    init setup

---
+++ Algebra.HelloWorld.Web.MvcApp/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Algebra.HelloWorld.Web.MvcApp.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    [DisplayName("Naziv knjige")]
+    public string Name { get; set; }
+
+    [DisplayName("Posuđena?")]
+    public bool IsBorrowed { get; set; }
+
+    [DisplayName("Datum posudbe")]
+    public DateTime? DateTimeBorrowed { get; set; }
+}
