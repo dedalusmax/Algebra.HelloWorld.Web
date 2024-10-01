df71c1a81363a83e1fe9dc599f8542ecf746ef00
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-10-01 20:22:17 +0200

    tests and config

---
+++ Algebra.HelloWorld.UnitTests/SqlProviderTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Algebra.HelloWorld.UnitTests;
+
+public class SqlProviderTests
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlProviderTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Testing.json", true);
+
+        _configuration = builder.Build();
+    }
+
+    [Fact]
+    public void TestSql_Connection_Failed()
+    {
+        var connection = new SqlConnection(string.Empty);
+        
+        connection.Open();
+        connection.Close();
+
+        // TODO: systemInvalid
+        connection.Dispose();
+    }
+
+    [Fact]
+    public void TestSql_Connection_Success()
+    {
+        var connection = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=book_library;Trusted_Connection=true");
+
+        connection.Open();
+        connection.Close();
+
+        connection.Dispose();
+    }
+
+    [Fact]
+    public void TestSql_ConnectionScoped_Success()
+    {
+        using var connection = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=book_library;Trusted_Connection=true");
+
+        connection.Open();
+    }
+
+    [Fact]
+    public void TestSql_ConnectionNewer_Success()
+    {
+        using var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=book_library;Trusted_Connection=true;");
+
+        connection.Open();
+    }
+
+    [Fact]
+    public void TestSql_ConnectionSql_Failed()
+    {
+        using var connection = new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=book_library;User ID=sa;Password=ratko123;");
+
+        connection.Open();
+    }
+
+    [Fact]
+    public void TestSql_ConnectionConfiguration_Success()
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        using var connection = new SqlConnection(connectionString);
+
+        connection.Open();
+    }
+}
--- Algebra.HelloWorld.Web.MvcApp/Controllers/HomeController.cs
+++ Algebra.HelloWorld.Web.MvcApp/Controllers/HomeController.cs
@@ -7,14 +7,18 @@ namespace Algebra.HelloWorld.Web.MvcApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
             return View();
         }
 
