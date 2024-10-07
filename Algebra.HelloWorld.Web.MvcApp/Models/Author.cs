c9f2c983b208a474502dcab145afdfad6ea44ff7
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-10-07 20:36:46 +0200

    clean

---
+++ Algebra.HelloWorld.Web.MvcApp/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Algebra.HelloWorld.Domain.Models;
+
+public class Author 
+{
+    public int AuthorId { get; set; }
+
+    [DisplayName("Autor")]
+    public string Name { get; set; }
+
+    public string Bio { get; set; }
+
+    public override string ToString() => Name;
+}
---
+++ Algebra.HelloWorld.Web.MvcApp/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using Algebra.HelloWorld.Domain.Interfaces;
+using Algebra.HelloWorld.Domain.Models;
+
+namespace Algebra.HelloWorld.Data.Repositories;
+
+public class BookRepository : IBookRepository
+{
+    private readonly string _connectionString = string.Empty;
+
+    public BookRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+
+    public void Create(Book book)
+    {
+        //_books!.Add(book);
+    }
+
+    public void Update(Book book)
+    {
+        //var data = _books!.SingleOrDefault(x => x.Id == book.Id);
+        //if (data != null)
+        //{
+        //    data.Name = book.Name;
+        //    data.Author = book.Author;
+        //    data.IsBorrowed = book.IsBorrowed;
+        //    data.DateTimeBorrowed = book.DateTimeBorrowed;
+        //}
+    }
+
+    public void Delete(Book book)
+    {
+        //var data = _books!.SingleOrDefault(x => x.Id == book.Id);
+        //if (data != null)
+        //{
+        //    _books!.Remove(data);
+        //}
+    }
+
+    public List<Book> GetBooks()
+    {
+        return new List<Book>();
+    }
+
+    public Book? GetById(int id)
+    {
+        return new Book();
+    }
+}
